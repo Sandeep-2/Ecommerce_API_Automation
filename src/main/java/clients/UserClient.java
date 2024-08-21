7a6a79ca85954cf12814b58ac750f68e260c1bd8
Sandeep-2 <sandeepsurapalli25@gmail.com>
2024-08-21 11:20:01 +0530

    1. Establishing a Client Layer for API Calls

---
+++ src/main/java/clients/UserClient.java
@@ -0,0 +1,46 @@
+package clients;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import utilities.PropertyUtils;
+import utilities.RandomEmailGenerator;
+import io.restassured.path.json.JsonPath;
+
+public class UserClient {
+    private static String baseUrl;
+    private static String randomEmail;
+
+    public UserClient() {
+        baseUrl = PropertyUtils.getProperty("base.url");
+        randomEmail = RandomEmailGenerator.generateRandomEmail();
+    }
+
+    public Response createUser() {
+        RestAssured.baseURI = baseUrl;
+        String requestBody = String.format("{\"email\": \"%s\", \"password\": \"12345678\"}", randomEmail);
+
+        Response response = RestAssured.given().contentType(ContentType.JSON).body(requestBody)
+                .post("/api/auth/signup");
+
+        return response;
+    }
+
+    public Response authenticateUser() {
+        RestAssured.baseURI = baseUrl;
+        String loginRequestBody = String.format("{\"email\": \"%s\", \"password\": \"12345678\"}", randomEmail);
+
+        Response response = RestAssured.given().contentType(ContentType.JSON).body(loginRequestBody)
+                .post("/api/auth/login");
+
+        return response;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public String getRandomEmail() {
+        return randomEmail;
+    }
+}
--- src/test/java/SignupAPITest.java
+++ src/test/java/SignupAPITest.java
@@ -1,3 +1,4 @@
+import clients.UserClient;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -12,43 +13,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SignupAPITest {
 
-    static String baseUrl;
-    static int statusCode;
-    static String randomEmail;
+    static UserClient userClient;
 
     @BeforeMethod
     public static void startTest() {
-        baseUrl = PropertyUtils.getProperty("base.url");
+        userClient = new UserClient();
     }
 
     @Test
     public static void createNewAccount() {
-        RestAssured.baseURI = baseUrl;
-        randomEmail = RandomEmailGenerator.generateRandomEmail();
-        String requestBody = String.format("{\"email\": \"%s\", \"password\": \"12345678\"}", randomEmail);
 
-        Response response = RestAssured.given().contentType(ContentType.JSON).body(requestBody)
-                .post("/api/auth/signup");
+        Response response = userClient.createUser();
 
-        statusCode = response.getStatusCode();
+        int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 201, "Expected status code is not matched.");
-        assertThat(response.jsonPath().getString("data.user.email"), Matchers.equalTo(randomEmail));
+        assertThat(response.jsonPath().getString("data.user.email"), Matchers.equalTo(userClient.getRandomEmail()));
     }
 
     @Test(dependsOnMethods = "createNewAccount")
-    public static void userLoginWithValidCredentials(){
-        RestAssured.baseURI = baseUrl;
-        String loginRequestBody = String.format("{\"email\": \"%s\", \"password\": \"12345678\"}", randomEmail);
-
-        Response response = RestAssured.given().contentType(ContentType.JSON).body(loginRequestBody)
-                .post("/api/auth/login");
-
+    public static void userLoginWithValidCredentials() {
+        Response response = userClient.authenticateUser();
 
         assertThat(response.getStatusCode(), Matchers.is(200));
         assertThat(response.jsonPath().get("data"), Matchers.notNullValue());
         assertThat(response.jsonPath().get("data.user"), Matchers.notNullValue());
         assertThat(response.jsonPath().get("data.session"), Matchers.notNullValue());
-        assertThat(response.jsonPath().getString("data.user.email"), Matchers.equalTo(randomEmail));
+        assertThat(response.jsonPath().getString("data.user.email"), Matchers.equalTo(userClient.getRandomEmail()));
         assertThat(response.jsonPath().getString("data.session.token_type"), Matchers.equalTo("bearer"));
         assertThat(response.jsonPath().getString("data.session.refresh_token"), Matchers.notNullValue());
         assertThat(response.jsonPath().getString("data.user.id"), Matchers.equalTo(response.jsonPath().getString("data.session.user.id")));
